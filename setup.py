13f080a4b00bcbcb32e48a39c4e099dfc0bb310f
Thomas Levine <_@thomaslevine.com>
2014-02-24 07:47:18 +0000

    setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(name='special_snowflake',
+    author='Thomas Levine',
+    author_email='_@thomaslevine.com',
+    description='Find the unique indices for a dataset',
+    url='https://github.com/tlevine/special_snwoflake.git',
+    classifiers=[
+        'Intended Audience :: Developers',
+    ],
+    packages=['special_snowflake'],
+    install_requires = ['sliding_window'],
+    tests_require = ['nose'],
+    version='0.0.1',
+    license='AGPL',
+)
