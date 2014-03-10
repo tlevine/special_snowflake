0de2ef8da51a0b9376739aa642314f152503b75a
Thomas Levine <_@thomaslevine.com>
2014-03-10 22:52:32 +0000

    test on a real dataset

--- special_snowflake/test/test_api.py
+++ special_snowflake/test/test_api.py
@@ -1,3 +1,4 @@
+import os
 from io import StringIO
 
 import nose.tools as n
@@ -15,17 +16,9 @@ def test_fromdicts_defaults():
     n.assert_equal(observed, expected)
 
 def test_fromcsv_defaults():
-
-@n.nottest
-def test_fromcsv_comma():
-    fp = StringIO('''color,shape,meows\r
-pink,square,no\r
-green,square,no\r
-yellow,cat,yes\r
-''')
-    fp.seek(0)
-    observed = fromcsv(fp)
-    expected = {('color',)}
+    with open(os.path.join('special_snowflake','test','fixtures','carte-des-licencies-sportifs-dans-les-hauts-de-seine.csv'), 'r') as fp:
+        observed = fromcsv(fp)
+    assert False, observed
     n.assert_set_equal(observed, expected)
 
 @n.nottest
