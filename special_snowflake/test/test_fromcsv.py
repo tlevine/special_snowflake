a2b708166f8728deef6fd7737b69b13a2d115d2a
Thomas Levine <_@thomaslevine.com>
2014-03-10 22:59:42 +0000

    aoeu

--- special_snowflake/test/test_fromcsv.py
+++ special_snowflake/test/test_fromcsv.py
@@ -7,8 +7,5 @@ def test_fromcsv():
     def fake_separator(*args, **kwargs):
         return ['a','b'],[{'a':3,'b':9}]
 
-    def fake_fromdicts(header, data, n_columns, only_adjacent):
-        return {('a',), ('b',), ('a','b')}
-
-    observed = _fromcsv(fake_separator, fake_fromdicts, None, None, None, [], {})
-    n.assert_set_equal(observed, {('a',), ('b',), ('a','b')})
+    observed = _fromcsv(fake_separator, None, {})
+    assert False, observed
