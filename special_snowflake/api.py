682d6905a97158ede002c7fc03697f6db3a87922
Thomas Levine <_@thomaslevine.com>
2014-03-10 22:40:37 +0000

    api

--- special_snowflake/api.py
+++ special_snowflake/api.py
@@ -1,8 +1,8 @@
-from fromcsv import _fromcsv
+from fromcsv import _fromcsv, _separate_fp
 from fromdicts import _fromdicts
 
 def fromcsv(fp, *csv_args, n_columns = 3, only_adjacent = True, **csv_kwargs):
-    return _fromcsv(fp, fromdicts, n_columns, only_adjacent, csv_args, csv_kwargs):
+    return _fromcsv(_separate_fp, fromdicts, fp, n_columns, only_adjacent, csv_args, csv_kwargs)
 
 def fromdicts(header, data, n_columns = 3, only_adjacent = True):
     return _fromdicts(header, data, n_columns, only_adjacent)
