b8333e9ab7df35761632e3c353fccd263c6153e1
Thomas Levine <_@thomaslevine.com>
2014-03-10 22:02:33 +0000

    multicol hash on a missing key

--- special_snowflake/test.py
+++ special_snowflake/test.py
@@ -3,18 +3,24 @@ import special_snowflake
 import special_snowflake._snowflake
 import special_snowflake._load
 
-def test_multicol_hash():
-    row = {'a':8,'b':3,'c':10}
-    observed = special_snowflake._snowflake.multicol_hash(row, ('a','b'))
-    expected = hash((8,3))
-    n.assert_equal(observed, expected)
-
 data = [
     {'a':8, 'b': 3,'c':13,'d': 8},
     {'a':88,'b': 1,'c':10,'d': 8},
     {'a':8, 'b':23,'c':10,'d': 8},
 ]
 headers = ('a','b','c','d')
+
+def test_multicol_hash():
+    row = {'a':8,'b':3,'c':10}
+    observed = special_snowflake._snowflake.multicol_hash(row, ('a','b'))
+    expected = hash((8,3))
+    n.assert_equal(observed, expected)
+
+def test_multicol_hash_on_missing_key():
+    row = {'a':8,'b':3,'c':10}
+    observed = special_snowflake._snowflake.multicol_hash(row, ('a','d'))
+    expected = hash((8,None))
+    n.assert_equal(observed, expected)
 
 def test_snowflake_1_adjacent():
     observed = special_snowflake.fromdicts(headers, data, n_columns = 1, only_adjacent = True)
