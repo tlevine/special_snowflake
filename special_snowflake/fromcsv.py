070ea60fea9be75e0be34fd49acc1afe9ba5284a
Thomas Levine <_@thomaslevine.com>
2014-03-10 22:39:33 +0000

    aoeu

--- special_snowflake/fromcsv.py
+++ special_snowflake/fromcsv.py
@@ -1,8 +1,6 @@
 import csv
 import json
 
-from special_snowflake._snowflake import _fromdicts
-
 def _separate_fp(fp, *args, **kwargs):
     '''
     Return a tuple of
@@ -13,6 +11,6 @@ def _separate_fp(fp, *args, **kwargs):
     data = csv.DictReader(fp, *args, **kwargs)
     return header, data
 
-def _fromcsv(fp, fromdicts, n_columns, only_adjacent, csv_args, csv_kwargs):
+def _fromcsv(separate_fp, fromdicts, fp, n_columns, only_adjacent, csv_args, csv_kwargs):
     header, data = separate_fp(fp, *csv_args, **csv_kwargs)
     return fromdicts(header, data, n_columns, only_adjacent)
--- special_snowflake/test/test_fromcsv.py
+++ special_snowflake/test/test_fromcsv.py
@@ -1,3 +1,18 @@
+import nose.tools as n
+
+from special_snowflake.fromcsv import _fromcsv
+
+def fake_separator(*args, **kwargs):
+    return ['a','b'],[{'a':3,'b':9}]
+
+def fake_fromdicts(header, data, n_columns, only_adjacent):
+    return {('a',), ('b',), ('a','b')}
+
+def test_fromcsv():
+    observed = _fromcsv(fake_separator, fake_fromdicts, None, None, None, [], {})
+    n.assert_set_equal(observed, {('a',), ('b',), ('a','b')})
+
+@n.nottest
 def test_fromcsv_comma():
     fp = StringIO('''color,shape,meows\r
 pink,square,no\r
@@ -5,10 +20,11 @@ green,square,no\r
 yellow,cat,yes\r
 ''')
     fp.seek(0)
-    observed = special_snowflake._load.fromcsv(fp)
+    observed = _fromcsv(fp)
     expected = {('color',)}
     n.assert_set_equal(observed, expected)
 
+@n.nottest
 def test_fromcsv_semicolon():
     fp = StringIO('''color;shape;meows
 pink;square;no
@@ -16,6 +32,6 @@ green;square;no
 yellow;square;yes
 ''')
     fp.seek(0)
-    observed = special_snowflake._load.fromcsv(fp, delimiter = ';', n_columns = 1)
+    observed = _fromcsv(fp, delimiter = ';', n_columns = 1)
     expected = {('color',)}
     n.assert_set_equal(observed, expected)
