ac25cd15a19e329618534368ebeb547c124b7617
Thomas Levine <_@thomaslevine.com>
2014-03-11 11:47:10 +0000

    weird

--- special_snowflake/test/test_fromcsv.py
+++ special_snowflake/test/test_fromcsv.py
@@ -5,7 +5,7 @@ from special_snowflake.fromcsv import _separate_fp
 
 def test_separate_fp():
     with open(os.path.join('special_snowflake','test','fixtures','carte-des-licencies-sportifs-dans-les-hauts-de-seine.csv')) as fp:
-        header, data = _separate_fp(fp)
-    assert False, next(data)
+        header, data = _separate_fp(fp, delimiter = ';')
+        assert False, (header, next(data))
 #   observed = _None, {})
     assert False, observed
