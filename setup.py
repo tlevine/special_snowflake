1a3d013db35e04e511fc0f9810fb5303f6efeeb5
Thomas Levine <_@thomaslevine.com>
2014-03-11 15:10:44 +0000

    setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python3
 from distutils.core import setup
 
+import special_snowflake as s
+
 setup(name='special_snowflake',
-    author='Thomas Levine',
-    author_email='_@thomaslevine.com',
-    description='Find the unique indices for a dataset',
-    url='https://github.com/tlevine/special_snwoflake.git',
-    classifiers=[
+    author = 'Thomas Levine',
+    author_email = '_@thomaslevine.com',
+    description = 'Find the unique indices for a dataset',
+    url = 'https://github.com/tlevine/special_snwoflake.git',
+    classifiers = [
         'Intended Audience :: Developers',
     ],
-    packages=['special_snowflake'],
+    packages = ['special_snowflake'],
     install_requires = ['sliding_window'],
     tests_require = ['nose'],
-    version='0.0.3',
-    license='AGPL',
+    version = s.__version,
+    license = 'AGPL',
 )
---
+++ special_snowflake/__init__.py
@@ -0,0 +1,4 @@
+from api import fromcsv, fromdicts
+
+__version__ = '0.0.3'
+__author__ = 'Thomas Levine <_@thomaslevine.com>'
