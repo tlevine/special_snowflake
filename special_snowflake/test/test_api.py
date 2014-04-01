7da22a32af6c331420981928872b9aefeb3db041
Thomas Levine <_@thomaslevine.com>
2014-04-01 16:01:33 +0000

    oooh

--- special_snowflake/test/test_api.py
+++ special_snowflake/test/test_api.py
@@ -16,13 +16,28 @@ def test_fromdicts_defaults():
     expected = {('b',),('a','c')}
     n.assert_equal(observed, expected)
 
-def test_fromcsv_semicolon():
+def test_fromcsv_stringio():
     fp = StringIO('''color;shape;meows
 pink;square;no
 green;square;no
 yellow;square;yes
 ''')
     observed = fromcsv(fp, delimiter = ';', n_columns = 1)
+    expected = {('color',)}
+    n.assert_set_equal(observed, expected)
+
+def test_fromcsv_file():
+    path = os.path.join('special_snowflake','test','fixtures','carte-des-licencies-sportifs-dans-les-hauts-de-seine.csv')
+    with open(path, 'r') as fp:
+        observed = fromcsv(fp, delimiter = ';', n_columns = 1)
+    expected = {('color',)}
+    n.assert_set_equal(observed, expected)
+
+def test_fromresponse():
+    path = os.path.join('special_snowflake','test','fixtures','carte-des-licencies-sportifs-dans-les-hauts-de-seine.p')
+    with open(path, 'rb') as fp:
+        response = pickle.load(fp)
+    observed = fromresponse(response, delimiter = ';', n_columns = 1)
     expected = {('color',)}
     n.assert_set_equal(observed, expected)
 
