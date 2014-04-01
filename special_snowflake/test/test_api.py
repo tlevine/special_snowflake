d16b2ea6e1c87b77ce967782af8be43d9a90146d
Thomas Levine <_@thomaslevine.com>
2014-04-01 15:58:38 +0000

    testcase

--- special_snowflake/test/test_api.py
+++ special_snowflake/test/test_api.py
@@ -1,5 +1,6 @@
 import os
 from io import StringIO
+import pickle
 
 import nose.tools as n
 
@@ -22,5 +23,13 @@ green;square;no
 yellow;square;yes
 ''')
     observed = fromcsv(fp, delimiter = ';', n_columns = 1)
+    expected = {('color',)}
+    n.assert_set_equal(observed, expected)
+
+def test_fromresponse():
+    path = os.path.join('special_snowflake','test','fixtures','carte-des-licencies-sportifs-dans-les-hauts-de-seine.p')
+    with open(path, 'rb') as fp:
+        response = pickle.load(fp)
+    observed = fromcsv(response, delimiter = ';', n_columns = 1)
     expected = {('color',)}
     n.assert_set_equal(observed, expected)
