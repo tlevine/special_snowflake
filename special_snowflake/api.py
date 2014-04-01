8984837ff586d34442c3912c83b1297ce5e147fe
Thomas Levine <_@thomaslevine.com>
2014-04-01 15:44:14 +0000

    change defaults

--- special_snowflake/api.py
+++ special_snowflake/api.py
@@ -3,7 +3,7 @@ import os
 from special_snowflake.fromcsv import _separate_fp
 from special_snowflake.fromdicts import _fromdicts
 
-def fromcsv(fp, n_columns = 3, only_adjacent = True, **csv_kwargs):
+def fromcsv(fp, n_columns = 3, only_adjacent = False, **csv_kwargs):
     position = fp.tell()
     if fp.seek(0, os.SEEK_END) == 0:
         return set()
@@ -12,5 +12,5 @@ def fromcsv(fp, n_columns = 3, only_adjacent = True, **csv_kwargs):
     header, data = _separate_fp(fp, **csv_kwargs)
     return _fromdicts(header, data, n_columns, only_adjacent)
 
-def fromdicts(header, data, n_columns = 3, only_adjacent = True):
+def fromdicts(header, data, n_columns = 3, only_adjacent = False):
     return _fromdicts(header, data, n_columns, only_adjacent)
