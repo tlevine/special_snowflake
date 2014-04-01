f08dcbf91d8cf10febd228929c31f496f7c9ce22
Thomas Levine <_@thomaslevine.com>
2014-04-01 15:55:31 +0000

    fromresponse

--- special_snowflake/api.py
+++ special_snowflake/api.py
@@ -1,4 +1,5 @@
 import os
+import io
 
 from special_snowflake.fromcsv import _separate_fp
 from special_snowflake.fromdicts import _fromdicts
@@ -11,6 +12,10 @@ def fromcsv(fp, n_columns = 3, only_adjacent = False, **csv_kwargs):
     fp.seek(position, os.SEEK_SET)
     header, data = _separate_fp(fp, **csv_kwargs)
     return _fromdicts(header, data, n_columns, only_adjacent)
+
+def fromresponse(response, n_columns = 3, only_adjacent = False, **csv_kwargs):
+    fp = io.StringIO(response.text)
+    return fromcsv(fp, n_columns = n_columns, only_adjacent = only_adjacent, **csv_kwargs)
 
 def fromdicts(header, data, n_columns = 3, only_adjacent = False):
     return _fromdicts(header, data, n_columns, only_adjacent)
