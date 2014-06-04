617e684f5f25521f659556e07edbc26933ba1091
Thomas Levine <_@thomaslevine.com>
2014-06-04 07:30:34 -0400

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python3
 from distutils.core import setup
 
-import special_snowflake.meta as s
-
 setup(name='special_snowflake',
     author = 'Thomas Levine',
     author_email = '_@thomaslevine.com',
     description = 'Find the unique indices for a dataset',
-    url = 'https://github.com/tlevine/special_snwoflake.git',
+    url = 'https://github.com/tlevine/special_snowflake.git',
     classifiers = [
         'Intended Audience :: Developers',
     ],
     packages = ['special_snowflake'],
-    install_requires = ['sliding_window'],
+    install_requires = ['sliding-window'],
     tests_require = ['nose'],
     version = '0.0.9',
     license = 'AGPL',
